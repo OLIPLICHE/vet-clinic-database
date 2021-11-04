67a556cef49551d350caa0e1444ad54a87ef1689
olipliche <olipliche@gmail.com>
2021-11-04 21:26:07 +0100

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals(
   id INT PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
@@ -9,3 +11,30 @@ CREATE TABLE animals(
 
 ALTER TABLE animals 
 ADD species TEXT;
+
+CREATE TABLE owners(
+  id SERIAL PRIMARY KEY,                    
+  full_name TEXT,   
+  age  INT
+);
+
+CREATE TABLE species(
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+ALTER TABLE animals
+  DROP species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) 
+REFERENCES species (id);
+
+ALTER TABLE animals 
+ADD COLUMN owners_id INT,
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owners_id)
+REFERENCES owners (id);
+
