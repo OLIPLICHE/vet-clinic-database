2736f631cc746aa50ed3d67e7f93ec2d326f7f94
olipliche <olipliche@gmail.com>
2021-11-05 16:36:54 +0100

    Update schema file

--- schema.sql
+++ schema.sql
@@ -38,3 +38,25 @@ ADD CONSTRAINT fk_owners
 FOREIGN KEY (owners_id)
 REFERENCES owners (id);
 
+CREATE TABLE vets(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INT,
+  date_of_graduate DATE
+);
+
+CREATE TABLE specializations(
+  species_id INT,
+  vets_id INT,
+  PRIMARY KEY (species_id, vets_id),
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits(
+  animals_id INT,
+  vets_id INT,
+  date DATE,
+  CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
